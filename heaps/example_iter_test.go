6125c5e2d494a82b44aae0530998f560fcd22966
houz42 <jijiaoqi@gmail.com>
2023-12-14 14:52:43 +0800

    feat(heaps): iterator

---
+++ heaps/example_iter_test.go
@@ -0,0 +1,22 @@
+//go:build goexperiment.rangefunc
+
+package heaps_test
+
+import (
+	"fmt"
+
+	"github.com/houz42/abstract/heaps"
+)
+
+func ExampleHeap_PopAll() {
+	h := heaps.New(9, 5, 2, 7)
+	for i, v := range h.PopAll() {
+		fmt.Println(i, v)
+	}
+
+	// Output:
+	// 0 2
+	// 1 5
+	// 2 7
+	// 3 9
+}
---
+++ heaps/heaps_iter.go
@@ -0,0 +1,18 @@
+//go:build goexperiment.rangefunc
+
+package heaps
+
+import "iter"
+
+// PopAll returns an iterator that pops elements in the heap one by one.
+func (h *Heap[E]) PopAll() iter.Seq2[int, E] {
+	return func(yield func(int, E) bool) {
+		i := 0
+		for h.Len() > 0 {
+			if !yield(i, h.Pop()) {
+				return
+			}
+			i++
+		}
+	}
+}
