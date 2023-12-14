90b507a704f4845ef05ce030bf07249e92800eca
houz42 <jijiaoqi@gmail.com>
2023-12-14 14:42:08 +0800

    feat(heaps): heaps package

---
+++ heaps/example_test.go
@@ -0,0 +1,84 @@
+package heaps_test
+
+import (
+	"fmt"
+
+	"github.com/houz42/abstract/heaps"
+)
+
+func Example() {
+	h := heaps.New(2, 1, 5, 6)
+	h.Push(3)
+	h.Remove(3)
+	fmt.Printf("minimum: %d\n", h.Top())
+	for h.Len() > 0 {
+		fmt.Printf("%d ", h.Pop())
+	}
+
+	// Output:
+	// minimum: 1
+	// 1 2 3 5
+}
+
+func Example_priorityQueue() {
+	type process struct {
+		pid      int
+		niceness int
+	}
+
+	queue := heaps.NewFunc[process](func(x, y process) bool { return x.niceness < y.niceness })
+
+	queue.Push(process{pid: 1, niceness: -20})
+	queue.Push(process{pid: 2, niceness: 0})
+	queue.Push(process{pid: 3, niceness: 10})
+	queue.Push(process{pid: 4, niceness: -1})
+
+	for queue.Len() > 0 {
+		p := queue.Pop()
+		fmt.Printf("start process %d with niceness %d\n", p.pid, p.niceness)
+	}
+
+	// Output:
+	// start process 1 with niceness -20
+	// start process 4 with niceness -1
+	// start process 2 with niceness 0
+	// start process 3 with niceness 10
+}
+
+func ExampleHeap_Remove() {
+	h := heaps.New(1, 5, 3, 2)
+	fmt.Println("removed: ", h.Remove(2))
+
+	for h.Len() > 0 {
+		fmt.Println(h.Pop())
+	}
+
+	// Output:
+	// removed: 3
+	// 1
+	// 2
+	// 5
+}
+
+func ExampleHeap_Reverse() {
+	type plan struct {
+		name     string
+		severity int
+	}
+
+	queue := heaps.NewFunc[plan](func(x, y plan) bool { return x.severity < y.severity }).Reverse()
+
+	queue.Push(plan{name: "call friend", severity: 1})
+	queue.Push(plan{name: "finish report", severity: 3})
+	queue.Push(plan{name: "buy food", severity: 2})
+
+	for queue.Len() > 0 {
+		plan := queue.Pop()
+		fmt.Printf("%d: %s\n", plan.severity, plan.name)
+	}
+
+	// Output:
+	// 3: finish report
+	// 2: buy food
+	// 1: call friend
+}
---
+++ heaps/heaps.go
@@ -0,0 +1,104 @@
+// Package heaps provides a heap implementation for any type.
+// A heap is a tree with the property that each node is the
+// minimum-valued (or maximum if reversed) node in its subtree.
+//
+// The minimum (maximum) element in the tree is the root, at index 0.
+//
+// A heap is a common way to implement a priority queue. To build a priority queue,
+// pass in a less method which orders the elements by their priorities,
+// so Push adds items while Pop removes the highest-priority item from the queue.
+// See the example for more details.
+package heaps
+
+import (
+	"cmp"
+	"container/heap"
+)
+
+// Heap is a tree with the property that each node is the minimum-valued (or maximum if reversed)
+// node in its subtree.
+type Heap[E any] struct {
+	impl *heapImpl[E]
+}
+
+// New creates a new heap for ordered element types.
+// The initial values are optional.
+func New[E cmp.Ordered](values ...E) *Heap[E] {
+	return NewFunc(func(x, y E) bool { return x < y }, values...)
+}
+
+// NewFunc creates a new heap for any type.
+// The initial values are optional.
+func NewFunc[E any](less func(x, y E) bool, values ...E) *Heap[E] {
+	impl := &heapImpl[E]{
+		values: values,
+		less:   less,
+	}
+	heap.Init(impl)
+	return &Heap[E]{impl: impl}
+}
+
+// Reverse returns a new Heap in which the elements will be pop out in reserved sequence to the original one
+func (h *Heap[E]) Reverse() *Heap[E] {
+	r := &Heap[E]{
+		impl: &heapImpl[E]{
+			values: make([]E, 0, len(h.impl.values)),
+			less:   func(x, y E) bool { return h.impl.less(y, x) },
+		},
+	}
+	copy(r.impl.values, h.impl.values)
+	heap.Init(r.impl)
+
+	return r
+}
+
+// Len returns number of elements in the heap.
+func (h *Heap[E]) Len() int { return len(h.impl.values) }
+
+// Push pushes the element x onto the heap.
+// The complexity is O(log n) where n = h.Len().
+func (h *Heap[E]) Push(x E) {
+	heap.Push(h.impl, x)
+}
+
+// Pop removes and returns the first element from the heap.
+// The complexity is O(log n) where n = h.Len().
+// Pop is equivalent to [Remove](h).
+func (h *Heap[E]) Pop() E {
+	v := heap.Pop(h.impl)
+	return v.(E)
+}
+
+// Top returns the first element from the heap.
+// The complexity is O(1).
+func (h *Heap[E]) Top() E {
+	return h.impl.values[0]
+}
+
+// Remove removes and returns the element at index i from the heap.
+// The complexity is O(log n) where n = h.Len().
+func (h *Heap[E]) Remove(i int) E {
+	v := heap.Remove(h.impl, i)
+	return v.(E)
+}
+
+type heapImpl[E any] struct {
+	values []E
+	less   func(x, y E) bool
+}
+
+func (h *heapImpl[E]) Len() int           { return len(h.values) }
+func (h *heapImpl[E]) Less(i, j int) bool { return h.less(h.values[i], h.values[j]) }
+func (h *heapImpl[E]) Swap(i, j int)      { h.values[i], h.values[j] = h.values[j], h.values[i] }
+
+func (h *heapImpl[E]) Push(x any) {
+	h.values = append(h.values, x.(E))
+}
+
+func (h *heapImpl[E]) Pop() any {
+	old := h.values
+	n := len(old)
+	x := old[n-1]
+	h.values = old[0 : n-1]
+	return x
+}
