c646a0fbfe0220b89f55d569707349932515791e
houz42 <jijiaoqi@gmail.com>
2023-12-26 18:20:39 +0800

    fix(skiplist): rename methods

--- skiplists/skiplists.go
+++ skiplists/skiplists.go
@@ -71,21 +71,26 @@ func (sl *SkipList[V]) Reverse() *SkipList[V] {
 // Len returns number of elements in the SkipList
 func (sl *SkipList[V]) Len() int { return sl.size }
 
-// Search returns an element and true if it is in the SkipList,
+// Get returns an element and true if it is in the SkipList,
 // otherwise zero value of type V and false.
 // The returned value is useful when V is a custom type and the provided `cmp` method may
-// return 0 (means equal) for different values, e.g. `cmp` only compares one field of a struct.
-func (sl *SkipList[V]) Search(val V) (V, bool) {
+// return 0 (means equal) for different values, e.g., `cmp` only compares one field of a struct.
+func (sl *SkipList[V]) Get(val V) (V, bool) {
 	node := sl.head
 
-	level := min(sl.level-1, sl.bestEntryLevel())
+	level := min(sl.level, sl.bestEntryLevel()) - 1
 	for ; level >= 0; level-- {
 		for node.next[level] != nil && sl.cmp(node.next[level].val, val) < 0 {
 			node = node.next[level]
 		}
+		if node.next[level] != nil && sl.cmp(node.next[level].val, val) == 0 {
+			node = node.next[level]
+			goto CHECK
+		}
 	}
 	node = node.next[0]
 
+CHECK:
 	if node == nil || sl.cmp(node.val, val) != 0 {
 		var v V
 		return v, false
@@ -93,9 +98,9 @@ func (sl *SkipList[V]) Search(val V) (V, bool) {
 	return node.val, true
 }
 
-// Insert inserts an element into the SkipList.
+// Set inserts an element into the SkipList.
 // If the element is already in, the element will be overwritten with the input value.
-func (sl *SkipList[V]) Insert(val V) {
+func (sl *SkipList[V]) Set(val V) {
 	// nodes in each level just before the target
 	updates := make([]*node[V], sl.level)
 
@@ -120,6 +125,8 @@ func (sl *SkipList[V]) Insert(val V) {
 	}
 
 	newLevel := sl.randomLevel()
+
+	// add new levels if needed
 	if newLevel > sl.level {
 		updates = append(updates, make([]*node[V], newLevel-sl.level)...)
 		jumps = append(jumps, make([]int, newLevel-sl.level)...)
@@ -134,6 +141,7 @@ func (sl *SkipList[V]) Insert(val V) {
 		sl.level = newLevel
 	}
 
+	// insert the new node to each level
 	node := &node[V]{
 		val:   val,
 		width: make([]int, newLevel),
@@ -162,7 +170,7 @@ func (sl *SkipList[V]) Insert(val V) {
 
 // Delete deletes an element from the SkipList.
 // If the value is not found, nothing happens.
-func (sl *SkipList[V]) Delete(val V) {
+func (sl *SkipList[V]) Delete(val V) V {
 	updates := make([]*node[V], sl.level)
 
 	node := sl.head
@@ -175,9 +183,11 @@ func (sl *SkipList[V]) Delete(val V) {
 	node = node.next[0]
 
 	if node == nil || sl.cmp(node.val, val) != 0 {
-		return
+		var v V
+		return v
 	}
 
+	// delete node from each level
 	for level := 0; level < sl.level; level++ {
 		if updates[level].next[level] == node {
 			updates[level].width[level] += node.width[level] - 1
@@ -187,6 +197,7 @@ func (sl *SkipList[V]) Delete(val V) {
 		}
 	}
 
+	// remove higher levels contains nothing
 	for sl.level > 1 && sl.head.next[sl.level-1] == nil {
 		sl.level--
 	}
@@ -194,6 +205,8 @@ func (sl *SkipList[V]) Delete(val V) {
 	sl.head.width = sl.head.width[:sl.level]
 
 	sl.size--
+
+	return node.val
 }
 
 // At returns the i-th element in the SkipList.
@@ -203,6 +216,10 @@ func (sl *SkipList[V]) At(i int) V {
 		panic(fmt.Errorf("runtime error: index out of range [%d] with skip list length %d", i, sl.size))
 	}
 
+	if i == 0 {
+		return sl.head.next[0].val
+	}
+
 	node := sl.head
 	pos := 0
 	for level := sl.level - 1; level >= 0; level-- {
@@ -210,13 +227,17 @@ func (sl *SkipList[V]) At(i int) V {
 			pos += node.width[level]
 			node = node.next[level]
 		}
+		if pos == i {
+			return node.val
+		}
 	}
 
 	return node.val
 }
 
 // UpdateAt updates the value at the specified index in the SkipList.
-// It panics with a runtime error if the index is out of range.
+// It panics with a runtime error if the index is out of range,
+// or the inserted new value will violate the SkipList's ordering property.
 func (sl *SkipList[V]) UpdateAt(i int, val V) {
 	if i < 0 || i >= sl.size {
 		panic(fmt.Errorf("runtime error: index out of range [%d] with skip list length %d", i, sl.size))
@@ -225,18 +246,28 @@ func (sl *SkipList[V]) UpdateAt(i int, val V) {
 	node := sl.head
 	pos := 0
 	for level := sl.level - 1; level >= 0; level-- {
-		for node != nil && pos+node.width[level] <= i {
+		for node != nil && pos+node.width[level] < i {
 			pos += node.width[level]
 			node = node.next[level]
 		}
 	}
 
-	node.val = val
+	target := node.next[0]
+	next := target.next[0]
+
+	if sl.cmp(node.val, val) >= 0 {
+		panic("updated value is less than or equal to the previous one")
+	}
+	if next != nil && sl.cmp(val, next.val) >= 0 {
+		panic("updated value is greater than or equal to the next one")
+	}
+
+	target.val = val
 }
 
 // DeleteAt deletes i-th element in the SkipList.
 // It panics if i is not valid, just like accessing slice element with an out-of-range index.
-func (sl *SkipList[V]) DeleteAt(i int) {
+func (sl *SkipList[V]) DeleteAt(i int) V {
 	if i < 0 || i >= sl.size {
 		panic(fmt.Errorf("runtime error: index out of range [%d] with skip list length %d", i, sl.size))
 	}
@@ -271,14 +302,16 @@ func (sl *SkipList[V]) DeleteAt(i int) {
 	sl.head.width = sl.head.width[:sl.level]
 
 	sl.size--
+
+	return node.val
 }
 
 func (sl *SkipList[V]) bestEntryLevel() int {
-	return bits.Len64(uint64(sl.size + 1))
+	return bits.Len64(uint64(sl.size)) + 1
 }
 
 func (sl *SkipList[V]) randomLevel() int {
-	maxLevel := bits.Len64(uint64(sl.size + 1))
-	level := bits.TrailingZeros64(rand.Uint64())
-	return int(min(level, maxLevel)) + 1
+	maxLevel := sl.bestEntryLevel()
+	level := bits.TrailingZeros64(rand.Uint64() | (1 << (maxLevel)))
+	return level + 1
 }
